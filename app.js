41c35ae73204a07b80b8cc279a35dc45a7a9cabb
dpjenk <dpjenk@outlook.com>
2021-12-20 17:03:26 -0600

    second commit

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ const pages = document.querySelector("#pages");
 const checkbox = document.querySelector("#checkbox");
 const submit = document.querySelector(".btn");
 const list = document.querySelector("#book-list");
+const trash = document.querySelector(".trash"); 
 
 // ADD EVENT LISTENER
 
 submit.addEventListener('click', function(e) {
-    e.preventDefault();
+    e.preventDefault();  // Prevents actual submit
 
     if(title.value === '' || author.value === '' || pages.value === '') {
         alert("Please complete all fields");
@@ -30,18 +31,37 @@ submit.addEventListener('click', function(e) {
         newRow.appendChild(newPagesColumn);
         newPagesColumn.innerText = pages.value;
 
-        
         const newReadColumn = document.createElement('td');
         newRow.appendChild(newReadColumn);
         if (checkbox.checked === true) {
             newReadColumn.innerText = "Yes";
         } else {
             newReadColumn.innerText = "No"
-        } 
+        }   
         
+        const newTrashColumn = document.createElement('td');
+        newRow.appendChild(newTrashColumn);
+        newTrashColumn.insertAdjacentHTML('beforeend', '<i class="far fa-trash-alt trash"></i>');
     }
 
+    // CLEAR ALL INPUT FIELDS AFTER SUBMISSION
+ title.value = '';
+ author.value = '';
+ pages.value = '';
+ checkbox.checked = false;
 
+   
+});
+ 
+// DELETE BOOK INFO FROM BOOK LIST
 
+document.querySelector('#book-list').addEventListener('click', function(e) {
+    deleteBook(e.target)
+});
+        
+function deleteBook(e) {
+    if(e.classList.contains('trash')) {
+        e.parentElement.parentElement.remove();
+    }
+}
 
-})
